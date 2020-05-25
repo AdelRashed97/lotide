d9f47fbf646d0853055316868aa535a7f40a2510
Adel Rashed <adel_rashed@outlook.com>
2020-05-25 23:38:44 +0000

    Added emoji and follow ES6 literal

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+function assertEqual(actual,expected){
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, '1');
+assertEqual(1, 5);
+assertEqual('apple','apple');
