e47855cc1221f8aa7b8fc74bdc651a99ad37cf50
Adel Rashed <adel_rashed@outlook.com>
2020-05-26 00:15:19 +0000

    returns a new array without the first element

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arry) {
+  if (arry.length === 0 || arry.length === 1) {
+    return [];
+  } else {
+    return arry.slice(1);// return a new array starting with the second element
+  }
+};
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
