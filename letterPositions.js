01884c2aef957bcdabd233f585a8002139e4efa7
Adel Rashed <adel_rashed@outlook.com>
2020-05-27 20:16:35 +0000

    created a loop that access each letter of string and skips spaces

---
+++ letterPositions.js
@@ -0,0 +1,31 @@
+const eqArrays = function(arry1,arry2) {
+  // if arrays are of different length, return false
+  if (arry1.length !== arry2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arry1.length; i++) {
+      // return false if  both arrays differ by an element
+      if (arry1[i] !== arry2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arry1,arry2) {
+  const result = eqArrays(arry1,arry2);
+  result ? console.log('✅✅✅ Assertion Passed: Arrays are Equal') : console.log('🛑🛑🛑 Assertion Failed: Arrays are NOT equal');
+};
+
+const letterPositions = function(string) {
+  const results = {};
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] !== " ") {
+      console.log(string[i]);
+    }
+  }
+
+};
+
+letterPositions("hello world");
