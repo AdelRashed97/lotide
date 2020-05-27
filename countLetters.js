86aee73c2e98f02f5fa0d11815d890ba75f5329e
Adel Rashed <adel_rashed@outlook.com>
2020-05-27 19:42:13 +0000

    created a loop inside countLetters to access and print each letter inside a string

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(string) {
+  for(let letter of string) {
+    console.log(letter);
+  }
+}
+
+// test case
+countLetters("lighthouse in the house");
