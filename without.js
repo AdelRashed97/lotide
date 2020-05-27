b9ecf877901de7b1955d05db281704ba2debbb7a
Adel Rashed <adel_rashed@outlook.com>
2020-05-27 00:21:25 +0000

    fixed eslint errors

--- without.js
+++ without.js
@@ -15,32 +15,36 @@ const eqArrays = function(arry1,arry2) {
 
 const assertArraysEqual = function(arry1,arry2) {
   const result = eqArrays(arry1,arry2);
-  result ? console.log('✅✅✅ Assertion Passed: Arrays are Equal') : console.log('🛑🛑🛑 Assertion Failed: Arrays are NOT equal'); 
+  result ? console.log('✅✅✅ Assertion Passed: Arrays are Equal') : console.log('🛑🛑🛑 Assertion Failed: Arrays are NOT equal');
 };
 
-const without = function (sourceArry,itemsToRemove) {
+const without = function(sourceArry,itemsToRemove) {
   // if itemsToRemove is empty ,return original array
   if (itemsToRemove.length === 0) {
     return sourceArry;
   } else {
     let outArry = [];
     for (let element of sourceArry) {
-      for (elementToRemove of itemsToRemove) {
-        if (element !== elementToRemove) {
-          outArry.push(element);
-        }
+      if (!itemsToRemove.includes(element)) {
+        outArry.push(element);
       }
 
     }
     return outArry;
   }
 };
-
+// test case
 assertArraysEqual(without([1, 2, 3],[]), [1, 2, 3]); // => should PASS
 assertArraysEqual(without([1, 2, 3],[2]) ,[1, 2, 3]); // => should PASS
 assertArraysEqual(without([1, 2, 3],[2,3]), [1, 2, 3]); // => should PASS
 
-let numbers =[1,2,3,9,8,9,2,9,1];
+let numbers = [1,2,3,9,8,9,2,9,1];
 let numbers2 = without(numbers,[1,2,9]);
+console.log(numbers);
 console.log(numbers2);
-assertArraysEqual(numbers,[1,2,3,9,8,9,2,9,1]);
+assertArraysEqual(numbers,[1,2,3,9,8,9,2,9,1]);// should pass
+
+const words = ["hello", "world", "lighthouse"];
+const words2 = without(words, ["lighthouse"]);
+console.log(words);// test that without does not alter original array
+console.log(words2);
