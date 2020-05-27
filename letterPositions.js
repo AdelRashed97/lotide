b3941bb621b4ee101682dad51f966185314e565a
Adel Rashed <adel_rashed@outlook.com>
2020-05-27 20:25:00 +0000

    inside the loop, created an if statement that creates a key for each letter inside an object and the value for  each key is an array of numbers that represent the positions at which the letter appears in

--- letterPositions.js
+++ letterPositions.js
@@ -22,8 +22,13 @@ const letterPositions = function(string) {
   const results = {};
   for (let i = 0; i < string.length; i++) {
     if (string[i] !== " ") {
-      console.log(string[i]);
+      if (results[string[i]]) {
+        results[string[i]].push(i);
+      } else {
+        results[string[i]] = [i];
+      }
     }
+    console.log(results)
   }
 
 };
