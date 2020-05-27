8b112cb9706b24588543ec4636d09ad2e2795fc2
Adel Rashed <adel_rashed@outlook.com>
2020-05-27 16:44:52 +0000

    added test cases for middle.js

--- middle.js
+++ middle.js
@@ -35,4 +35,14 @@ const middle = function(arry) {
   }
 
 };
+// test cases
+assertArraysEqual(middle([1]) , []);
+assertArraysEqual(middle([1,2]) , []);
+assertArraysEqual(middle([1,2,3]) , [2]);
+assertArraysEqual(middle([1,2,3,4]) , [2,3]);
+
+assertArraysEqual(middle(['bob']) , []);
+assertArraysEqual(middle(['bob','jack']) , []);
+assertArraysEqual(middle(['bob','jack','sam']) , ['jack']);
+assertArraysEqual(middle(['bob','jack','sam','mike']) , ['jack','sam']);
 
