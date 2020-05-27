ff61b1ea9e09019ab3c415727a7c0efeeb96a25a
Adel Rashed <adel_rashed@outlook.com>
2020-05-27 00:19:08 +0000

    passed test cases

---
+++ without.js
@@ -0,0 +1,46 @@
+const eqArrays = function(arry1,arry2) {
+  // if arrays are of different length, return false
+  if (arry1.length !== arry2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arry1.length; i++) {
+      // return false if  both arrays differ by an element
+      if (arry1[i] !== arry2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arry1,arry2) {
+  const result = eqArrays(arry1,arry2);
+  result ? console.log('✅✅✅ Assertion Passed: Arrays are Equal') : console.log('🛑🛑🛑 Assertion Failed: Arrays are NOT equal'); 
+};
+
+const without = function (sourceArry,itemsToRemove) {
+  // if itemsToRemove is empty ,return original array
+  if (itemsToRemove.length === 0) {
+    return sourceArry;
+  } else {
+    let outArry = [];
+    for (let element of sourceArry) {
+      for (elementToRemove of itemsToRemove) {
+        if (element !== elementToRemove) {
+          outArry.push(element);
+        }
+      }
+
+    }
+    return outArry;
+  }
+};
+
+assertArraysEqual(without([1, 2, 3],[]), [1, 2, 3]); // => should PASS
+assertArraysEqual(without([1, 2, 3],[2]) ,[1, 2, 3]); // => should PASS
+assertArraysEqual(without([1, 2, 3],[2,3]), [1, 2, 3]); // => should PASS
+
+let numbers =[1,2,3,9,8,9,2,9,1];
+let numbers2 = without(numbers,[1,2,9]);
+console.log(numbers2);
+assertArraysEqual(numbers,[1,2,3,9,8,9,2,9,1]);
