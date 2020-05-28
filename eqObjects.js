de0c4a473662c262d78ec74484427ba2801086de
Adel Rashed <adel_rashed@outlook.com>
2020-05-28 01:00:57 +0000

    implement an if statement to test if values stored in objects are arrays or primitive

--- eqObjects.js
+++ eqObjects.js
@@ -49,32 +49,53 @@ const eqObjects = function(obj1,obj2) {
   // test if the value for each key in one object is the same as the value for that same key in the other object
 
   for (let key of keys1) {
-    if (obj1[key] !== obj2[key]) {
-      return false;
+    // check the type of value stored in key of object1
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      // check if both val1 and val 2 are arrays
+      if (!eqArrays(val1,val2)) {
+        return false;
+      }
+      
+
+    } else  {
+      // assume both values are of primitive type
+      if (val1 !== val2) {
+        return false;
+      }
+      
 
     }
-
-    
-
   }
 
   return true;
-
-
-
-
+  
 };
-
-
+    
+        
 // test case
-// test 1: test if both objects have same number of keys and key names;
+// test 1: test for perimitive values;
 const ab = { a: "1", b: "2" };
 const ba = { b: "2" , a: "1" };
 assertEqual(eqObjects(ab, ba),true); // => true
 
 const ac = { a: "1", c: "2" };
 const ca = { c: "4" , a: "1" };
-assertEqual(eqObjects(ac, ca),true); // => false
+assertEqual(eqObjects(ac, ca),false); // => false
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(abc, ab),false); // => false
+
+// test 2: test for array values;
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc),true); // => true
+
+const dc2 = { d: ["5", 3], c: "1" };
+assertEqual(eqObjects(cd, dc2),false); // => false
+
+const cd3 = { d: "1", c: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd3),false); // => false
