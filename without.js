62824641e1da16fcec791d6d8fb372b6a3d8bd77
Adel Rashed <adel_rashed@outlook.com>
2020-05-28 20:27:04 +0000

    updated assertArraysEqual

--- without.js
+++ without.js
@@ -38,7 +38,7 @@ const without = function(sourceArry,itemsToRemove) {
 };
 // test case
 assertArraysEqual(without([1, 2, 3],[]), [1, 2, 3]); // => should PASS
-assertArraysEqual(without([1, 2, 3],[2]) ,[1, 3]); // => should pass
+assertArraysEqual(without([1, 2, 3],[2]) ,[1,3]); // => should pass
 assertArraysEqual(without([1, 2, 3],[2,3]), [1]); // => should PASS
 
 let numbers = [1,2,3,9,8,9,2,9,1];
